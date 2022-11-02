43a53eb9dd69aad12964cb2959ea4b3dad018dee
Kevin Ewing <kewing@middlebury.edu>
2022-11-02 12:29:22 -0400

    Testing once more

---
+++ scroll.js
@@ -0,0 +1,13 @@
+AOS.init({
+    duration: 1200,
+})
+
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 58}%`;
+});
